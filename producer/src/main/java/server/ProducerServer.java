f624cc79bbe73c793034b2a54717b7cd0830a28c
Yadhukrishna S Pai <yadhukrishnap@sahajsoft.com>
2019-09-11 11:00:57 +0530

    added metrics for the application

--- producer/src/main/java/server/ProducerServer.java
+++ producer/src/main/java/server/ProducerServer.java
@@ -1,28 +1,36 @@
 package server;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import com.google.gson.Gson;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.docs.DocService;
+import com.linecorp.armeria.server.grpc.GrpcServiceBuilder;
+import io.grpc.ServerInterceptors;
+import io.prometheus.client.Collector;
+import io.prometheus.client.CollectorRegistry;
+import me.dinowernli.grpc.prometheus.Configuration;
+import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.linecorp.armeria.server.Server;
-import com.linecorp.armeria.server.ServerBuilder;
-import com.linecorp.armeria.server.grpc.GrpcServiceBuilder;
-import com.linecorp.armeria.server.docs.DocService;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import static server.ProducerEndpointImpl.idealBatchSize;
+import static server.ProducerEndpointImpl.lastBatchSize;
+import static spark.Spark.get;
 
 public class ProducerServer {
 	
 	
 	private static final Logger log = LoggerFactory.getLogger(ProducerServer.class);
-	
+
+    private static CollectorRegistry collectorRegistry = new CollectorRegistry();
 
     public static void main(String args[]) {
     	
@@ -39,11 +47,15 @@ public class ProducerServer {
     		System.exit(1);
     	}
     	
- 
+
+
         ServerBuilder sb = new ServerBuilder();
         sb.http(portNumber);          // Set port for Netty
 
         ProducerEndpointImpl producerEndpoint = new ProducerEndpointImpl(path);
+
+        MonitoringServerInterceptor monitoringInterceptor =
+                MonitoringServerInterceptor.create(Configuration.cheapMetricsOnly().withCollectorRegistry(collectorRegistry));
 
         Thread thread = new Thread(producerEndpoint);
         thread.start();
@@ -51,18 +63,73 @@ public class ProducerServer {
 
         sb.service(
                 new GrpcServiceBuilder()
-                        .addService(producerEndpoint)
+                        .addService(ServerInterceptors.intercept(producerEndpoint.bindService(), monitoringInterceptor))
                         .build()
         );
 
+
         sb.serviceUnder("/docs", new DocService());
+
         /* Docservice API endpoint
             Also acts like postman (But for GRPC)
         */
 
+
         Server server = sb.build();
         server.start().join();
+
+
+        get("/metrics", (req, res) -> {
+            res.type("application/json");
+
+            Enumeration<Collector.MetricFamilySamples> samples = findAllRecordedMetricOrThrow();
+            ArrayList<Collector.MetricFamilySamples> familySamplesArrayList = Collections.list(samples);
+
+            return new Gson().toJsonTree(setMetricList(familySamplesArrayList));
+        });
+
         // DUN DUN DUN!
+
+
+    }
+
+    private static Collector.MetricFamilySamples findRecordedMetricOrThrow(String name) {
+        return RegistryHelper.findRecordedMetricOrThrow(name, collectorRegistry);
+    }
+
+    static Enumeration<Collector.MetricFamilySamples> findAllRecordedMetricOrThrow() {
+        return RegistryHelper.findRecordedMetric(collectorRegistry);
+    }
+
+    public static List<Metric> setMetricList(List<Collector.MetricFamilySamples> sampleMetrics) {
+        List<Metric> metricList = new ArrayList<>();
+
+        for(Collector.MetricFamilySamples metricFamilySamples : sampleMetrics){
+
+            double sampleSize = 0;
+
+            if( metricFamilySamples.samples.size()>0 )
+                 sampleSize = metricFamilySamples.samples.get(0).value * idealBatchSize - (idealBatchSize - lastBatchSize);
+
+            Metric metric = new Metric(metricFamilySamples.name, metricFamilySamples.type.toString(), sampleSize);
+            metricList.add(metric);
+
+        }
+        return metricList;
+    }
+
+
+    public static class Metric{
+        String name;
+        String type;
+        double value;
+
+        public Metric(String name, String type, double value) {
+            this.name = name;
+            this.type = type;
+            this.value = value;
+        }
+
     }
     
     private static CommandLine getCommandLineParser(String args[]) {
