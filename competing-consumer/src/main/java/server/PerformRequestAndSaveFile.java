5d01b4ceef64d97bbbd3d14f85dafd34345b9fa7
Anirudh Ramesh <anirudhr@sahajsoft.com>
2019-09-21 16:21:29 +0530

    Fix client side streaming

--- competing-consumer/src/main/java/server/PerformRequestAndSaveFile.java
+++ competing-consumer/src/main/java/server/PerformRequestAndSaveFile.java
@@ -3,129 +3,89 @@ package server;
 import java.io.File;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
 import com.linecorp.armeria.client.Clients;
 import com.sink.queue.GetMessageFromQueueGrpc;
 import com.sink.queue.GetMessageFromQueueGrpc.GetMessageFromQueueBlockingStub;
+import com.sink.queue.GetMessageFromQueueGrpc.GetMessageFromQueueStub;
 import com.sink.queue.RequestFromSink;
 import com.sink.queue.ResponseFromQueueSource;
 
-import io.grpc.stub.StreamObserver;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class PerformRequestAndSaveFile implements Runnable {
 
 	private static final Logger log = LoggerFactory.getLogger(PerformRequestAndSaveFile.class);
 
-	private GetMessageFromQueueGrpc.GetMessageFromQueueBlockingStub getMessageFromQueueStub;
+	private ManagedChannel managedChannel;
 
+	private GetMessageFromQueueBlockingStub getMessageFromQueueBlockingStub;
 	private RequestFromSink request;
 
-	private StreamObserver<ResponseFromQueueSource> responseStream;
-
 	// This is an expensive object. Creating 1 for thread an at the beginning.
-	private final ObjectMapper objectMapper = new ObjectMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
 	private ListenableFuture<ResponseFromQueueSource> future;
-	private boolean isQueueEmpty;
 
 	PerformRequestAndSaveFile(String endpointURL) {
 
-		this.getMessageFromQueueStub = getClientForEndPoint(endpointURL);
-
-		this.request = getRequestObject();
-//        this.responseStream = this.getStreamObserver();
-		isQueueEmpty = false;
-
-	}
-
-	private RequestFromSink getRequestObject() {
-		return RequestFromSink.newBuilder().build();
-	}
-
-	private GetMessageFromQueueBlockingStub getClientForEndPoint(String endpointURL) {
-		return Clients.newClient("gproto+http://" + endpointURL, GetMessageFromQueueBlockingStub.class);
+		managedChannel = ManagedChannelBuilder.forTarget(endpointURL).usePlaintext().build();
+		this.getMessageFromQueueBlockingStub = GetMessageFromQueueGrpc.newBlockingStub(managedChannel);
+		this.request = RequestFromSink.newBuilder().build();
 	}
 
 	@Override
 	public void run() {
 
-		Iterator<ResponseFromQueueSource> responseIterator = getIteratedResponseFromGRPCServer();
+		Iterator<ResponseFromQueueSource> responseIterator;
 
-		while (responseIterator.hasNext()) {
+		while (true) {
 
-			ResponseFromQueueSource responseFromQueueSource = responseIterator.next();
-			log.info("Got respones - {}", responseFromQueueSource);
-			List<ByteString> fileList = responseFromQueueSource.getFileList();
+			responseIterator = getMessageFromQueueBlockingStub.getItem(request);
 
-			if (fileList == null || fileList.size() == 0) {
-				isQueueEmpty = true;
-				continue;
-			}
+			while (responseIterator.hasNext()) {
 
-			for (ByteString file : fileList) {
-				ResponseJsonPojo responseJsonPojo = null;
-				try {
-					responseJsonPojo = objectMapper.readValue(file.toStringUtf8(), ResponseJsonPojo.class);
-					objectMapper.writeValue(new File(responseJsonPojo.getMessageId() + ".json"), responseJsonPojo);
-				} catch (Exception e) {
-					log.error("Error while parsing JSON / Writing to file for JSON - ", file.toStringUtf8(), e);
+				ResponseFromQueueSource responseFromQueueSource = responseIterator.next();
+				List<ByteString> fileList = responseFromQueueSource.getFileList();
+
+				for (ByteString byteString : fileList) {
+
+					try {
+						ResponseJsonPojo responseJsonPojo = objectMapper.readValue(byteString.toStringUtf8(),
+								ResponseJsonPojo.class);
+						objectMapper.writeValue(new File(responseJsonPojo.getMessageId() + ".json"), responseJsonPojo);
+					} catch (Exception e) {
+						log.error("Error while parsing JSON / Writing to file for JSON - ", byteString.toStringUtf8(),
+								e);
+					}
 				}
 
 			}
 
+//                        fileList.parallelStream().forEach(byteString -> {
+//                            ResponseJsonPojo responseJsonPojo = null;
+//                            try {
+//                                responseJsonPojo = objectMapper.readValue(byteString.toStringUtf8(), ResponseJsonPojo.class);
+//                                objectMapper.writeValue(new File(responseJsonPojo.getMessageId() + ".json"), responseJsonPojo);
+//                            } catch (Exception e) {
+//                                log.error("Error while parsing JSON / Writing to file for JSON - ", byteString.toStringUtf8(), e);
+//                            }
+//
+//                        });
 		}
 
-	}
 
-	private Iterator<ResponseFromQueueSource> getIteratedResponseFromGRPCServer() {
-		return this.getMessageFromQueueStub.getItem(request);
 	}
-
-//    private StreamObserver<ResponseFromQueueSource> getStreamObserver() {
-//        return new StreamObserver<ResponseFromQueueSource>() {
-//
-//            @Override
-//            public void onNext(ResponseFromQueueSource result) {
-//
-//                List<ByteString> fileList = result.getFileList();
-//
-//                if(fileList == null || fileList.size() == 0) {
-//                    log.warn("Cannot fetch anymore items - Queue is Empty");
-//                    isQueueEmpty = true;
-//                }
-//
-//
-//                for (ByteString byteString : fileList) {
-//
-//                    try {
-//                        ResponseJsonPojo responseJsonPojo = objectMapper.readValue(byteString.toStringUtf8(), ResponseJsonPojo.class);
-//                        objectMapper.writeValue(new File(responseJsonPojo.getMessageId() + ".json"), responseJsonPojo);
-//
-//                    } catch (Exception e) {
-//                        log.error("Error while parsing JSON / Writing to file for JSON - ", byteString.toStringUtf8(), e);
-//                    }
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onError(Throwable t) {
-//                log.error("Error while getting STREAMED response - ", t);
-//                isQueueEmpty = true;
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//                isQueueEmpty = true;
-//                log.info("Finished getting STREAMED response from gRPC endpoint, " +
-//                        "All files MUST be transffered to local disk");
-//            }
-//
-//        };
-//    }
 }
